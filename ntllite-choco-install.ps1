44cd20c8edea4fcbe6b27347abff51424f252b15
marknakamura <57322698+marknakamura@users.noreply.github.com>
2024-02-28 19:13:52 -0500

    Add files via upload

---
+++ ntllite-choco-install.ps1
@@ -0,0 +1,10 @@
+﻿Write-Host -ForegroundColor Green "Starting Chocolatey install now!"
+
+Get-ExecutionPolicy -List
+Start-Sleep -Seconds 15
+
+Set-ExecutionPolicy Unrestricted -Force
+Start-Sleep -Seconds 10
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Start-Sleep -Seconds 10
